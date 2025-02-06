3c4a4a6f3f603a489667589a2376f1bfa6a95a5f
David Bannon <dbannon@internode.on.net>
2025-02-06 16:24:30 +1100

    Added File listing, ctrl-c cleanup etc

--- httpserver/simplehttpserver.pas
+++ httpserver/simplehttpserver.pas
@@ -9,10 +9,13 @@ program simplehttpserver;
   is ideal to serve up simple web pages on my logger.
   Notes : I needed to copy   ../echo/webmodule/wmecho.*  into dir before building
           I added a default of index.html
+          If we land in a dir without index.html, give a list of files in there.
+          Handle trailing slash as it should.
           I disabled the (very questionable) use of FCount to kill the server after
             serving five files. Who knows why this was here.
-          Added (back) code to determine service dir and port on command line.
-  DRB, 2024-12-07
+          Added a means to terminate gracefull, Ctrl-C or SIGTERM
+          Added a touch of help on command line.
+  DRB, 2025-02-06
 }
 
 {$mode objfpc}{$H+}
@@ -22,7 +25,8 @@ uses
   {$IFDEF UNIX}{$IFDEF UseCThreads}
   cthreads,
   {$ENDIF}{$ENDIF}
-  sysutils, Classes, fphttpserver, fpmimetypes, wmecho;
+  sysutils, Classes, fphttpserver, fpmimetypes, wmecho,
+  BaseUnix;       // for Signal Names  Hmm, windows ?  No idea !
 
 Type
 
@@ -34,6 +38,8 @@ Type
 //    FCount : Integer;
     FMimeLoaded : Boolean;
     FMimeTypesFile: String;
+    procedure GenerateFileList(FFName: string; AResp: TFPHTTPConnectionResponse);
+    function MyAppendPathDelim(APath: string): string;
     procedure SetBaseDir(const AValue: String);
   Protected
     Procedure DoIdle(Sender : TObject);
@@ -50,6 +56,8 @@ Type
 
 Var
   Serv : TTestHTTPServer;
+  ExitNow : boolean;           // A semaphore set when ctrl-C received
+
 { TTestHTTPServer }
 
 procedure TTestHTTPServer.SetBaseDir(const AValue: String);
@@ -63,6 +71,7 @@ end;
 procedure TTestHTTPServer.DoIdle(Sender: TObject);
 begin
   // Writeln('Idle, waiting for connections');
+    if ExitNow then Serv.Active := False;      // Shutdown by a signal
 end;
 
 procedure TTestHTTPServer.CheckMimeLoaded;
@@ -74,49 +83,130 @@ begin
     end;
 end;
 
+function TTestHTTPServer.MyAppendPathDelim(APath : string) : string;    // here to avoid use lazfileutils
+begin
+    if APath[length(APath)] <> PathDelim then
+        Result := APath + PathDelim
+    else Result := APath;
+end;
+
+procedure TTestHTTPServer.GenerateFileList(FFName : string; AResp : TFPHTTPConnectionResponse);   // doppy, this should be a stream ....
+var
+    STL : tstringlist;
+    Info : TSearchRec;
+begin
+    STL := TStringList.Create();
+    STL.Insert(0, '<html><body><h1>File List</h1>');
+    Stl.Add('<table>');
+    If FindFirst (FFName + '*',faAnyFile, Info)=0 then begin
+        Stl.Add('<tr>');
+        repeat
+            if (Info.Name = '.') or (Info.Name = '..') then continue;
+            if (Info.Attr and faDirectory) = faDirectory then
+                Stl.add('<td>Directory</td><td><a href="' + Info.Name + '/">' + Info.Name + '</td>')
+            else begin
+                Stl.add('<td>' + inttostr(Info.size) + '</td><td><a href="'+ Info.Name + '">' + Info.Name + '</a></td>');
+            end;
+            Stl.add('</tr>');
+        until FindNext(info) <> 0;
+    end;
+    Stl.add('</table>');
+    FindClose(Info);
+    Stl.Add('</body></html>');
+    AResp.Content := StL.Text;
+    STL.Free;
+    AResp.SendContent;
+end;
+
 procedure TTestHTTPServer.HandleRequest(var ARequest: TFPHTTPConnectionRequest;
   var AResponse: TFPHTTPConnectionResponse);
-
 Var
   F : TFileStream;
   FN : String;
 
-begin
-  FN:=ARequest.Url;
-  if FN = '/' then
-    FN := '/index.html';                                // DRB
-  If (length(FN)>0) and (FN[1]='/') then
-    Delete(FN,1,1);
-  DoDirSeparators(FN);
-  FN:=BaseDir+FN;
-  if FileExists(FN) then
+    // Looks at URL, if it can make it work by adding an index.html or a '/' (ie dir) does so,
+    // returns True. If it returns false, nothing we can do, missing file or dir.
+    function CheckURL() : boolean;
     begin
-    // writeln(' TTestHTTPServer.HandleRequest and file exits.');
-    F:=TFileStream.Create(FN,fmOpenRead);
-    try
-      CheckMimeLoaded;
-      AResponse.ContentType:=MimeTypes.GetMimeType(ExtractFileExt(FN));
-      Writeln('Serving file: "',Fn,'". Reported Mime type: ',AResponse.ContentType);
-      AResponse.ContentLength:=F.Size;
-      AResponse.ContentStream:=F;
-      AResponse.SendContent;
-      AResponse.ContentStream:=Nil;
-    finally
-      F.Free;
+            Result := True;
+            if (length(FN)>0) and (FN[1]='/') then
+                Delete(FN,1,1);
+            DoDirSeparators(FN);       // Make a path contain appropiate seperators.
+            if FileExists(BaseDir + FN) then begin
+                FN:=BaseDir+FN;
+                exit(true);            // user has entered a usable filename
+            end;
+            if FileExists(BaseDir + FN + 'index.html') then begin
+                FN:=BaseDir+FN + 'index.html';
+                exit(true);            // a dir that does contain an index.html
+            end;
+            if DirectoryExists(BaseDir+FN) then begin
+                FN := MyAppendPathDelim(BaseDir+FN);
+                if FileExists(FN + 'index.html') then
+                    FN := FN + 'index.html';
+                exit(True);
+            end;
+            // If to here, user must have put an invalid file or dir in URL
+            result := False;
     end;
-    end
-  else
-    begin
-    Writeln('Failed to Serve file: "', Fn, '". Reported Mime type: ',AResponse.ContentType);
-    AResponse.Code:=404;
-    AResponse.SendContent;
-    end;
-//  Inc(FCount);              This was some auto shutdown, I have no idea why
-//  If FCount>=5 then
-//    Active:=False;
-end;
 
 begin
+    FN:=ARequest.Url;
+//    writeln('TTestHTTPServer.HandleRequest : User requested ', FN);
+    if not CheckURL() then begin
+        AResponse.Code:=404;                       // ToDo : this does not work
+        writeln('TTestHTTPServer.HandleRequest : File or Dir not found [', FN, ']');
+        AResponse.Content := '<html><body><h2>ERROR 404, File or Dir not found</h2></p>' + FN + '</p></body></html>';
+        AResponse.SendContent;
+        exit;            ;
+    end;
+    // Here, we believe we can help the user, either with a file or a dir list.
+    if (length(FN) = 0) or (FN[length(FN)] = PathDelim) then begin            // trailing Sep says is just a dir.
+//        Writeln('No file to serve, will do file list, FN=[', FN, ']');
+        GenerateFileList(FN, AResponse);
+        exit;
+    end;
+    // If to here, we seem to have a file to serve, do so !
+    // writeln(' TTestHTTPServer.HandleRequest and file exists.');
+    F:=TFileStream.Create(FN,fmOpenRead);
+    try
+        CheckMimeLoaded;
+        AResponse.ContentType:=MimeTypes.GetMimeType(ExtractFileExt(FN));
+//        Writeln('Serving file: "',Fn,'". Reported Mime type: ',AResponse.ContentType);
+        AResponse.ContentLength:=F.Size;
+        AResponse.ContentStream:=F;
+        AResponse.SendContent;
+        AResponse.ContentStream:=Nil;
+    finally
+        F.Free;
+    end;
+end;
+
+procedure HandleSigInt(aSignal: LongInt); cdecl;
+begin
+    case aSignal of
+        SigInt : Writeln('Ctrl + C used, will clean up and shutdown.');
+        SigTerm : writeln('TERM signal, will clean up and shutdown.');
+    else
+        begin writeln('Some signal received ??'); exit; end;
+    end;
+    ExitNow := True;            // Watched by the Idle method
+end;
+
+{begin
+     }
+
+begin
+    if FpSignal(SigInt, @HandleSigInt) = signalhandler(SIG_ERR) then begin
+        Writeln('Failed to install signal error: ', fpGetErrno);
+        exit;
+    end;
+    if FpSignal(SigTerm, @HandleSigInt) = signalhandler(SIG_ERR) then begin
+        Writeln('Failed to install signal error: ', fpGetErrno);
+        exit;
+    end;
+
+
   Serv:=TTestHTTPServer.Create(Nil);
   try
     Serv.BaseDir:=ExtractFilePath(ParamStr(0));
@@ -125,8 +215,13 @@ begin
     Serv.MimeTypesFile:='/etc/mime.types';
 {$endif}
     Serv.Port:=8080;
-    if ParamCount > 0 then
+    if ParamCount > 0 then begin
+        if (ParamStr(1) = '-h') or (ParamStr(1) = '--help') or (ParamStr(1) = 'help') then begin
+            writeln('Usage ', ExtractFileName(ParamStr(0)), ' [BaseDir] [Port]');
+            exit;
+        end;
       Serv.BaseDir:=ParamStr(1);
+    end;
     if ParamCount > 1 then
       Serv.Port:=StrToIntDef(ParamStr(2),8080);
     Serv.Threaded:=False;
